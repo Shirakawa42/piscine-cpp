ef08437ef2ec57e0548155beb3835ce26208cae2
shirakawa42 <shirakawa@live.fr>
2018-06-07 16:43:43 +0200

    d08ex02

--- day08/ex02/mutantstack.hpp
+++ day08/ex02/mutantstack.hpp
@@ -5,14 +5,27 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <stack>
 
-class	mutantstack
+template <typename T>
+class	MutantStack : public std::stack<T>
 {
+	private:
+		typedef std::stack<T>	_st;
 	public:
-		mutantstack();
-		~mutantstack();
-		mutantstack( const mutantstack & );
-		mutantstack &	operator = ( const mutantstack & );
+		MutantStack() {}
+		~MutantStack() {}
+		MutantStack( const MutantStack & cpy) : _st(cpy) {}
+		using	_st::operator = ;
+		typedef typename _st::container_type::iterator	iterator;
+		iterator	begin()
+		{
+			return _st::c.begin();
+		}
+		iterator	end()
+		{
+			return _st::c.end();
+		}
 };
 
 #endif
